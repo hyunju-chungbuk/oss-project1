3c99333a367b16f47a274cc5cd9fe80ee37087dd
lili10148- <lili10148-@chungbuk.ac.kr>
2025-10-29 17:10:37 +0900

    Test 브랜치에 코드를 추가함

--- SourceTreeTest.c
+++ SourceTreeTest.c
@@ -2,8 +2,14 @@
 
 int main()
 {
+    int i = 0;
+
     printf("First oss Code\n");
     printf("Second oss Code\n");
+
+    for (int i=0; i < 10; i++){
+        printf("Test 브랜치\n");
+    }
 
     return 0;
 }
