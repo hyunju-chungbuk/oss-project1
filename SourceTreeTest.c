dfd681bee5d5e2fb9b16ac3fdeb1894b4b66a377
lili10148- <lili10148-@chungbuk.ac.kr>
2025-10-29 17:22:31 +0900

    마스터 브랜치에 코드를 추가하였음

--- SourceTreeTest.c
+++ SourceTreeTest.c
@@ -2,14 +2,9 @@
 
 int main()
 {
-    int i = 0;
-
     printf("First oss Code\n");
     printf("Second oss Code\n");
-
-    for (int i=0; i < 10; i++){
-        printf("Test 브랜치\n");
-    }
+    printf("메인 브랜치에 코드를 추가하였음");
 
     return 0;
 }
