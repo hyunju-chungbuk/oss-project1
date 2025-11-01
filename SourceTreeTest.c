e2582c312172b88c709641f28d25dcc098cd6905
lili10148- <lili10148-@chungbuk.ac.kr>
2025-11-01 22:13:02 +0900

    Revert "Test 브랜치에 코드를 추가함"
    This reverts commit 3c99333a367b16f47a274cc5cd9fe80ee37087dd.

--- SourceTreeTest.c
+++ SourceTreeTest.c
@@ -2,14 +2,8 @@
 
 int main()
 {
-    int i = 0;
-
     printf("First oss Code\n");
     printf("Second oss Code\n");
-
-    for (int i=0; i < 10; i++){
-        printf("Test 브랜치\n");
-    }
 
     return 0;
 }
